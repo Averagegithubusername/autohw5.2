cb2c9197afe488c1a90744157e0734a1426bbb77
Averagegithubusername <s0mething_different@mail.ru>
2022-04-07 15:45:18 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/7jadmqy7yqc62a8i?svg=true)](https://ci.appveyor.com/project/Averagegithubusername/autohw5-2)
---
+++ src/test/java/test/WebAuthorisationTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+import static data.DataGenerator.Registration.getRegisteredUser;
+import static data.DataGenerator.Registration.getUser;
+import static data.DataGenerator.getRandomLogin;
+import static data.DataGenerator.getRandomPassword;
+
+public class WebAuthorisationTest {
+}
